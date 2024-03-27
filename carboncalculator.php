a0283114e5f990dd1defb643f3d110e0f35a7dcb
Ngoyarez <brianngoya764@yahoo.com>
2024-03-27 18:42:46 +0300

    Carbon Calculator

---
+++ carboncalculator.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Link of CSS files -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <link rel="stylesheet" href="assets/css/remixicon.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/owl-theme-default-min.css">
+        <link rel="stylesheet" href="assets/css/odometer.min.css">
+        <link rel="stylesheet" href="assets/css/fancybox.css">
+        <link rel="stylesheet" href="assets/css/aos.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="assets/css/dark-theme.css">
+        <title>Carbon Footprint Calculator</title>
+        <link rel="icon" type="image/png" href="assets/img/favicon.png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-... your integrity hash ..." crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <!-- Theme Switcher Start -->
+    <div class="switch-theme-mode">
+      <label id="switch" class="switch">
+        <input type="checkbox" onchange="toggleTheme()" id="slider" />
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <!-- Theme Switcher End -->
+
+    <!-- Breadcrumb Start -->
+    <div class="breadcrumb-wrap bg-f br-1">
+      <div class="container">
+        <div class="breadcrumb-title">
+            <h2>Carbon Footprint Calculator</h2>
+              <ul class="breadcrumb-menu list-style">
+                <li><a href="index.php">Home </a></li>
+                  <li>Carbon Footprint Calculator</li>
+              </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+    <div class="container ptb-100 offset-3">
+    <iframe width="710" height="1300" frameborder="2" marginwidth="0" marginheight="0" scrolling="no" src="https://calculator.carbonfootprint.com/calculator.aspx"></iframe>
+    </div>
+    
+</body>
+</html>
